647ca19c452ab63da5d89e5407a4f23b4b941307
William <307176637@qq.com>
2020-04-08 12:21:50 +0800

    重构了elements.py

--- elements.py
+++ elements.py
@@ -1,64 +1,155 @@
-class Chess:
-	def __init__(self, x, y, red=True, selected=False):
-		self.x = x
-		self.y = y
-		self.red = red
+from abc import abstractmethod
 
-	def position(self):  # 获取位置
-		return self.x, self.y
+
+class Chess:
+	def __init__(self, position, red=True, selected=False):
+		self.position = position
+		self.red = red
+		self.pos_list = []		# 将来的可能目标位置，由各派生类确定
+
+	@abstractmethod
+	def picture(self):
+		"""
+		返回该棋子的图片
+
+		:return:
+		"""
+		pass
+
+	@abstractmethod
+	def is_legal_action(self, dx, dy, end_position=None):
+		"""
+		判断此行为是否合法
+
+		:param dx: x方向的移动距离
+		:param dy: y方向的移动距离
+		:param end_position:
+		:return: 合法与否
+		"""
+		pass
+
+	@abstractmethod
+	def is_legal_move(self, end_position, chessboard):
+		"""
+		判断移动到某一位置是否合法
+
+		:param end_position: 将要移动到的位置
+		:param chessboard: 当前棋盘对象
+		:return: 合法与否
+		"""
+		pass
 
 	def set_position(self, pos):
-		self.x, self.y = pos
+		"""
+		Set the position of chess piece.
 
-	def d_position(self, start_position, end_position):  # 位移差
-		dx = end_position[0] - start_position[0]
-		dy = end_position[1] - start_position[1]
+		:param pos: (x, y)
+		:return:
+		"""
+		self.position = pos
+
+	def is_inplace(self):
+		"""
+		判断棋子是否位于己侧
+
+		:return: True: 在己侧；False: 不在己侧
+		"""
+		y = self.position[1]
+		if self.red and y > 4:
+			return False
+		elif not self.red and y < 5:
+			return False
+		return True
+
+	def d_position(self, end_position):
+		"""
+		计算移动到目标位置需要执行的动作
+
+		:param end_position: 目标位置
+		:return: (x轴移动动作, y轴移动动作)
+		"""
+		dx = end_position[0] - self.position[0]
+		dy = end_position[1] - self.position[1]
 		return dx, dy
 
-	def is_myplace(self, y):  # 判断兵,象是否过河
-		if self.red and y > 4:
-			return True
-		elif not self.red and y < 5:
-			return True
-		return False
+	@staticmethod
+	def is_out(pos):
+		"""
+		检查此坐标是否出棋盘
 
-	def is_out(self, pos):  # 检查是否越界
+		:param pos:
+		:return: True: 已出棋盘
+		"""
 		x, y = pos
 		if x < 0 or y < 0 or x > 8 or y > 9:
 			return False
 		return True
 
-	def position_has_chess(self, position, chessboard):  # 判断某个位置是否有棋子
-		x, y = position
-		if chessboard[x][y] == 0:
-			return False
-		return True
+	@staticmethod
+	def position_has_chess(position, chessboard):
+		"""
+		判断某个位置是否有棋子
 
-	def chess_is_my(self, red, chess):  # 判断是否为己方棋子
-		if red == chess.red:
+		:param position:
+		:param chessboard:
+		:return:
+		"""
+		x, y = position
+		if chessboard[x, y]:
 			return True
 		return False
 
-	def count_chess(self, start_position, end_position, chessboard):  # 计算起点和终点直线之间的棋子
-		x, y = start_position
-		z, w = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		sx = dx / abs(dx) if dx != 0 else 0
-		sy = dy / abs(dy) if dy != 0 else 0
+	def is_own(self, chess):
+		"""
+		判断是否为己方棋子
+
+		:param chess:
+		:return:
+		"""
+		if self.red == chess.red:
+			return True
+		return False
+
+	def count_chess(self, end_position, chessboard):
+		"""
+		计算到目标位置的直线上的棋子数
+
+		:param end_position:
+		:param chessboard:
+		:return:
+		"""
+		x, y = self.position
+		x_to, y_to = end_position
+		dx = 1 if x != x_to else 0
+		dy = 1 if y != y_to else 0
 		num = 0
-		while x != z or y != w:
-			x = int(x + sx)
-			y = int(y + sy)
-			if chessboard[x][y]:
+		while x != x_to or y != y_to:
+			x += dx
+			y += dy
+			if chessboard[x, y]:
 				num += 1
 		return num
 
+	def possible_move(self, chessboard):
+		"""
+		寻找所有可能移动到的合法位置
+
+		:param chessboard: 当前棋盘对象
+		:return: 一个合法移动位置的列表: [(0, 1), (2, 3), ...]
+		"""
+		legal_pos = []
+		for pos in self.pos_list:
+			if not self.is_out(pos) and self.is_legal_move(pos, chessboard):
+				legal_pos.append(pos)
+		return legal_pos
+
 
 class Bing(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Bing, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '兵'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 	def picture(self):
 		if self.red:
@@ -66,53 +157,47 @@ class Bing(Chess):
 		else:
 			return 'black_pawn.gif'
 
-	def rule(self, dx, dy):
+	def is_legal_action(self, dx, dy, end_position=None):
 		if abs(dx) + abs(dy) != 1:
 			return False
-		if self.is_myplace(self.y):  # 兵没有过河
-			if self.red and dy == -1:
+		if self.is_inplace():  # 兵没有过河
+			if (self.red and dy == 1 and dx == 0) or (not self.red and dy == -1 and dx == 0):
 				return True
-			if not self.red and dy == 1:
-				return True
-			return False
+			else:
+				return False
 		else:  # 兵已过河
-			if self.red and dy == 1:
+			if (self.red and dy == -1) or (not self.red and dy == 1):
 				return False
-			if not self.red and dy == -1:
-				return False
-			return True
+			else:
+				return True
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy):  # 走棋合法
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy):  # 走棋合法
 			if not self.position_has_chess(end_position, chessboard):  # 移动到的位置没棋子
 				return True
 			else:
-				if self.chess_is_my(self.red, chessboard[x][y]):  # 本方棋子，不能吃
+				if self.is_own(chessboard[x, y]):  # 本方棋子，不能吃
 					return False
 				else:  # 对方棋子，可以吃
 					return True
 		else:
 			return False
 
-	def try_move(self, position, chessboard):  # 返回棋子能够移动的所有位置
-		x, y = position
-		a = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
-
 
 class Che(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Che, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '车'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [
+			(x + 1, y), (x + 2, y), (x + 3, y), (x + 4, y), (x + 5, y), (x + 6, y), (x + 7, y), (x + 8, y),
+			(x - 1, y), (x - 2, y), (x - 3, y), (x - 4, y), (x - 5, y), (x - 6, y), (x - 7, y), (x - 8, y),
+			(x, y + 1), (x, y + 2), (x, y + 3), (x, y + 4), (x, y + 5), (x, y + 6), (x, y + 7), (x, y + 8),
+			(x, y + 9), (x, y - 1), (x, y - 2), (x, y - 3), (x, y - 4), (x, y - 5), (x, y - 6), (x, y - 7),
+			(x, y - 8), (x, y - 9)
+		]
 
 	def picture(self):
 		if self.red:
@@ -120,48 +205,36 @@ class Che(Chess):
 		else:
 			return 'black_rook.gif'
 
-	def rule(self, dx, dy):
+	def is_legal_action(self, dx, dy, end_position=None):
 		if not (dx == 0 and dy == 0):
 			if dx == 0 or dy == 0:
 				return True
 		return False
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy):
-			num = self.count_chess(start_position, end_position, chessboard)  # 路线上是否有子阻挡
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy):
+			num = self.count_chess(end_position, chessboard)  # 路线上是否有子阻挡
 			if not num:  # 没有子
 				return True
 			if num == 1 and self.position_has_chess(end_position, chessboard):  # 终点位置有子
-				if not self.chess_is_my(self.red, chessboard[x][y]):  # 不是本方子
+				if not self.is_own(chessboard[x, y]):  # 不是本方子
 					return True
 			return False
 		else:
 			return False
 
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 1, y), (x + 2, y), (x + 3, y), (x + 4, y), (x + 5, y), (x + 6, y), (x + 7, y), (x + 8, y),
-			 (x - 1, y), (x - 2, y), (x - 3, y), (x - 4, y), (x - 5, y), (x - 6, y), (x - 7, y), (x - 8, y),
-			 (x, y + 1), (x, y + 2), (x, y + 3), (x, y + 4), (x, y + 5), (x, y + 6), (x, y + 7), (x, y + 8),
-			 (x, y + 9), (x, y - 1), (x, y - 2), (x, y - 3), (x, y - 4), (x, y - 5), (x, y - 6), (x, y - 7),
-			 (x, y - 8), (x, y - 9)
-			 ]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
-
 
 class Ma(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Ma, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '马'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [
+			(x + 1, y + 2), (x + 2, y + 1), (x - 1, y + 2), (x - 2, y + 1),
+			(x - 1, y - 2), (x - 2, y - 1), (x + 1, y - 2), (x + 2, y - 1)
+		]
 
 	def picture(self):
 		if self.red:
@@ -169,53 +242,55 @@ class Ma(Chess):
 		else:
 			return 'black_knight.gif'
 
-	def rule(self, dx, dy):
+	def is_legal_action(self, dx, dy, end_position=None):
 		if dx == 0 or dy == 0:
 			return False
 		if abs(dx) + abs(dy) == 3:  # 马走日
 			return True
 
-	def can_move(self, start_position, end_position, chessboard):
-		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy) and not self.block(start_position, dx, dy, chessboard):
-			if not self.position_has_chess(end_position, chessboard):  # 不吃子
-				return True
-			else:  # 吃子
-				if not self.chess_is_my(self.red, chessboard[x][y]):  # 不是本方棋子
-					return True
-		return False
+	def block(self, dx, dy, chessboard):
+		"""
+		是否蹩马腿
 
-	def block(self, start_position, dx, dy, chessboard):  # 别马腿
-		x, y = start_position
+		:param dx:
+		:param dy:
+		:param chessboard:
+		:return: True: 蹩马腿
+		"""
+		x, y = self.position
 		if abs(dx) == 1:
 			y = int(y + dy / 2)
-			if chessboard[x][y] == 0:
+			if not chessboard[x, y]:
 				return False
 		else:
 			x = int(x + dx / 2)
-			if chessboard[x][y] == 0:
+			if not chessboard[x][y]:
 				return False
 		return True
 
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 1, y + 2), (x + 2, y + 1), (x - 1, y + 2), (x - 2, y + 1), (x - 1, y - 2), (x - 2, y - 1),
-			 (x + 1, y - 2), (x + 2, y - 1)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
+	def is_legal_move(self, end_position, chessboard):
+		x, y = end_position
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy) and not self.block(dx, dy, chessboard):
+			if not self.position_has_chess(end_position, chessboard):  	# 不吃子
+				return True
+			else:  														# 吃子
+				if not self.is_own(chessboard[x, y]):  # 不是本方棋子
+					return True
+		return False
 
 
 class Pao(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Pao, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '炮'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [
+			(x + 1, y), (x + 2, y), (x + 3, y), (x + 4, y), (x + 5, y), (x + 6, y), (x + 7, y), (x + 8, y),
+			(x - 1, y), (x - 2, y), (x - 3, y), (x - 4, y), (x - 5, y), (x - 6, y), (x - 7, y), (x - 8, y),
+			(x, y + 1), (x, y + 2), (x, y + 3), (x, y + 4), (x, y + 5), (x, y + 6), (x, y + 7), (x, y + 8),
+			(x, y + 9), (x, y - 1), (x, y - 2), (x, y - 3), (x, y - 4), (x, y - 5), (x, y - 6), (x, y - 7),
+			(x, y - 8), (x, y - 9)]
 
 	def picture(self):
 		if self.red:
@@ -223,44 +298,31 @@ class Pao(Chess):
 		else:
 			return 'black_cannon.gif'
 
-	def rule(self, dx, dy):
+	def is_legal_action(self, dx, dy, end_position=None):
 		if not (dx == 0 and dy == 0):
 			if dx == 0 or dy == 0:
 				return True
 		return False
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy):
-			num = self.count_chess(start_position, end_position, chessboard)
-			if not num:  # 移动的路线上没子
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy):
+			num = self.count_chess(end_position, chessboard)
+			if not num:  		# 移动的路线上没子
 				return True
 			if num == 2 and self.position_has_chess(end_position, chessboard):  # 有两个子且一个在终点处
-				if not self.chess_is_my(self.red, chessboard[x][y]):
+				if not self.is_own(chessboard[x, y]):
 					return True
 		return False
 
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 1, y), (x + 2, y), (x + 3, y), (x + 4, y), (x + 5, y), (x + 6, y), (x + 7, y), (x + 8, y),
-			 (x - 1, y), (x - 2, y), (x - 3, y), (x - 4, y), (x - 5, y), (x - 6, y), (x - 7, y), (x - 8, y),
-			 (x, y + 1), (x, y + 2), (x, y + 3), (x, y + 4), (x, y + 5), (x, y + 6), (x, y + 7), (x, y + 8), (x, y + 9),
-			 (x, y - 1), (x, y - 2), (x, y - 3), (x, y - 4), (x, y - 5), (x, y - 6), (x, y - 7), (x, y - 8), (x, y - 9)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
-
 
 class Shi(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Shi, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '士'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [(x + 1, y + 1), (x - 1, y + 1), (x - 1, y - 1), (x + 1, y - 1)]
 
 	def picture(self):
 		if self.red:
@@ -268,42 +330,32 @@ class Shi(Chess):
 		else:
 			return 'black_mandarin.gif'
 
-	def rule(self, dx, dy, end_position):
+	def is_legal_action(self, dx, dy, end_position=None):
 		x, y = end_position
 		if abs(dx) == 1 and abs(dy) == 1:
-			if self.red and 2 < x < 6 and 6 < y < 10:  # 判断是否出九宫格
+			if self.red and 2 < x < 6 < y < 10:  # 判断是否出九宫格
 				return True
 			if not self.red and 2 < x < 6 and -1 < y < 3:
 				return True
 		return False
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy, end_position):
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy, end_position):
 			if not self.position_has_chess(end_position, chessboard):  # 终点处无棋子
 				return True
-			elif not self.chess_is_my(self.red, chessboard[x][y]):  # 终点处是对方棋子
+			elif not self.is_own(chessboard[x, y]):  # 终点处是对方棋子
 				return True
 		return False
 
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 1, y + 1), (x - 1, y + 1), (x - 1, y - 1), (x + 1, y - 1)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
-
 
 class Shuai(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Shuai, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '帅'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]
 
 	def picture(self):
 		if self.red:
@@ -311,57 +363,42 @@ class Shuai(Chess):
 		else:
 			return 'black_king.gif'
 
-	def rule(self, dx, dy, end_position):
+	def is_legal_action(self, dx, dy, end_position=None):
 		x, y = end_position
 		if abs(dx) + abs(dy) == 1:
-			if self.red and 2 < x < 6 and 6 < y < 10:
+			if self.red and 2 < x < 6 < y < 10:
 				return True
 			if not self.red and 2 < x < 6 and -1 < y < 3:
 				return True
 		return False
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
+		dx, dy = self.d_position(end_position)
 		for i in chessboard:  # 判断是否有白脸将
-			for p in i:
-				if p == 0:
-					continue
-				if p.name == '帅':
-					if p.red != self.red:
-						a = p
-		x1, y1 = a.position()
-		if x1 == x:
-			flag = 0
-			for i in range(min(y1, y) + 1, max(y1, y)):
-				if chessboard[x1][i]:
-					flag = 1
-			if flag == 0:
-				return False
-		if self.rule(dx, dy, end_position):
+			if i.name == '帅':
+				x_enemy, y_enemy = i.position
+				if x_enemy == x:
+					for y_pos in range(min(y_enemy, y) + 1, max(y_enemy, y)):
+						if chessboard[x, y_pos]:
+							break
+					else:
+						return False
+				break
+		if self.is_legal_action(dx, dy, end_position):
 			if not self.position_has_chess(end_position, chessboard):  # 终点处无棋子
 				return True
-			elif not self.chess_is_my(self.red, chessboard[x][y]):  # 终点处是对方棋子
+			elif not self.is_own(chessboard[x, y]):  # 终点处是对方棋子
 				return True
 		return False
 
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
-
 
 class Xiang(Chess):
-	def __init__(self, x, y, red=True, selected=False):
-		super(Xiang, self).__init__(x, y, red=True, selected=False)
+	def __init__(self, position, red=True, selected=False):
+		super().__init__(position, red, selected)
 		self.name = '象'
-		self.red = red
+		x, y = self.position
+		self.pos_list = [(x + 2, y + 2), (x - 2, y + 2), (x - 2, y - 2), (x + 2, y - 2)]
 
 	def picture(self):
 		if self.red:
@@ -369,41 +406,36 @@ class Xiang(Chess):
 		else:
 			return 'black_elephant.gif'
 
-	def rule(self, dx, dy):
+	def is_legal_action(self, dx, dy, end_position=None):
+		x, y = end_position
 		if abs(dx) == 2 and abs(dy) == 2:
-			return True
+			if (self.red and y < 5) or (not self.red and y > 4):
+				return True
 		return False
 
-	def xiangti(self, start_position, end_position, dx, dy, chessboard):  # 是否别象眼或象过河
-		x, y = start_position
-		z, w = end_position
+	def block(self, dx, dy, chessboard):
+		"""
+		判断是否别象眼
+
+		:param dx:
+		:param dy:
+		:param chessboard:
+		:return:
+		"""
+		x, y = self.position
 		x = int(x + dx / 2)
 		y = int(y + dy / 2)
-		if chessboard[x][y] == 0:
+		if chessboard[x, y]:
+			return True
+		else:
 			return False
-		if self.red and w < 5:  # 象是否过河
-			return False
-		if not self.red and w > 4:
-			return False
-		return True
 
-	def can_move(self, start_position, end_position, chessboard):
+	def is_legal_move(self, end_position, chessboard):
 		x, y = end_position
-		dx, dy = self.d_position(start_position, end_position)
-		if self.rule(dx, dy) and not self.xiangti(start_position, end_position, dx, dy, chessboard):
+		dx, dy = self.d_position(end_position)
+		if self.is_legal_action(dx, dy, end_position) and not self.block(dx, dy, chessboard):
 			if not self.position_has_chess(end_position, chessboard):  # 终点处无棋子
 				return True
-			elif not self.chess_is_my(self.red, chessboard[x][y]):  # 终点处是对方棋子
+			elif not self.is_own(chessboard[x, y]):  # 终点处是对方棋子
 				return True
 		return False
-
-	def try_move(self, position, chessboard):
-		x, y = position
-		a = [(x + 2, y + 2), (x - 2, y + 2), (x - 2, y - 2), (x + 2, y - 2)]
-		b = []
-		for i in a:
-			if not self.is_out(i):
-				continue
-			if self.can_move(position, i, chessboard):
-				b.append(i)
-		return b
