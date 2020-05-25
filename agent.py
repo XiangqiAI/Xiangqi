7843fc2a7d3f31d88be96b2a4c7c21e0755becdb
William <307176637@qq.com>
2020-05-25 21:40:30 +0800

    changed the AI agent

--- agent.py
+++ agent.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import random
+from search import Search
 
 
 class AI:
 	@staticmethod
 	def get_move(game):										# 随机选择
-		moves = []
-		for move in game.get_legal_moves():
-			if game.can_move(move):
-				moves.append(move)
-		choice = random.choice(range(len(moves)))
-		return moves[choice]
+		# moves = []
+		# for move in game.get_legal_moves():
+		# 	if game.can_move(move):
+		# 		moves.append(move)
+		# choice = random.choice(range(len(moves)))
+		# return moves[choice]
+		search = Search()
+		return search.alpha_beta(game)
