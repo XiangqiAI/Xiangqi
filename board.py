dcd330a9caf7dfd4713147e3ed3257f96ae8a7ce
William <307176637@qq.com>
2020-05-27 19:04:20 +0800

    refactored some name

--- board.py
+++ board.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from elements import *
 from copy import deepcopy
+import torch
 
 
-class Game:
+class GameState:
 	"""
 	chessboard是一个棋盘，本质是一个字典
 	键为一个表示位置的元组，值为空或者一个棋子对象
@@ -15,7 +16,7 @@ class Game:
 
 	坐标原点在左上角，x轴向右，y轴向下
 	"""
-	def __init__(self, chessboard=None, red_move=True):
+	def __init__(self, chessboard=None, red_move=True, last_moves=None):
 		self.red_move = red_move			# 当前是否红方回合
 		if chessboard:
 			self.chessboard = chessboard
@@ -56,11 +57,77 @@ class Game:
 			self.chessboard[3, 0] = Shi((3, 0), red=False)
 			self.chessboard[5, 0] = Shi((5, 0), red=False)
 			self.chessboard[4, 0] = Shuai((4, 0), red=False)
+		self.index_map = {
+			'bing': 0,
+			'pao': 1,
+			'che': 2,
+			'ma': 3,
+			'xiang': 4,
+			'shi': 5,
+			'shuai': 6
+		}
+		if last_moves:
+			self.last_moves = last_moves
+		else:
+			self.last_moves = [
+				((-1, -1), (-1, -1)),
+				((-1, -1), (-1, -1)),
+				((-1, -1), (-1, -1)),
+				((-1, -1), (-1, -1)),
+				((-1, -1), (-1, -1))
+			]
+		self.all_moves = []
+		advisor_moves = [
+			((3, 0), (4, 1)), ((4, 1), (3, 0)), ((5, 0), (4, 1)), ((4, 1), (5, 0)),
+			((3, 2), (4, 1)), ((4, 1), (3, 2)), ((5, 2), (4, 1)), ((4, 1), (5, 2)),
+			((3, 9), (4, 8)), ((4, 8), (3, 9)), ((5, 9), (4, 8)), ((4, 8), (5, 9)),
+			((3, 7), (4, 8)), ((4, 8), (3, 7)), ((5, 7), (4, 8)), ((4, 8), (5, 7))
+		]
+		bishop_moves = [
+			((2, 0), (0, 2)), ((0, 2), (2, 0)), ((2, 4), (0, 2)), ((0, 2), (2, 4)),
+			((2, 0), (4, 2)), ((4, 2), (2, 0)), ((2, 4), (4, 2)), ((4, 2), (2, 4)),
+			((6, 0), (4, 2)), ((4, 2), (6, 0)), ((6, 4), (4, 2)), ((4, 2), (6, 4)),
+			((6, 0), (8, 2)), ((8, 2), (6, 0)), ((6, 4), (8, 2)), ((8, 2), (6, 4)),
+			((2, 9), (0, 7)), ((0, 7), (2, 9)), ((2, 5), (0, 7)), ((0, 7), (2, 5)),
+			((2, 9), (4, 7)), ((4, 7), (2, 9)), ((2, 5), (4, 7)), ((4, 7), (2, 5)),
+			((6, 9), (4, 7)), ((4, 7), (6, 9)), ((6, 5), (4, 7)), ((4, 7), (6, 5)),
+			((6, 9), (8, 7)), ((8, 7), (6, 9)), ((6, 5), (8, 7)), ((8, 7), (6, 5))
+		]
+		self.all_moves.extend(advisor_moves)
+		self.all_moves.extend(bishop_moves)
+		for i in range(9):
+			for j in range(10):
+				destinations = \
+					[(k, j) for k in range(9)] + \
+					[(i, k) for k in range(10)] + \
+					[(i + m, j + n) for (m, n) in
+						[(-2, -1), (-1, -2), (-2, 1), (1, -2), (2, -1), (-1, 2), (2, 1), (1, 2)]]
+				for (x, y) in destinations:
+					if (x, y) != (i, j) and x in range(9) and y in range(10):
+						self.all_moves.append(((i, j), (x, y)))
 
 	def copy(self):
 		chessboard = deepcopy(self.chessboard)
-		game = Game(chessboard, self.red_move)
+		last_moves = deepcopy(self.last_moves)
+		game = GameState(chessboard, self.red_move, last_moves)
 		return game
+
+	def state(self):
+		state = torch.zeros(20, 9, 10)
+		for piece in self.chessboard.values():
+			if piece:
+				x, y = piece.position
+				index = self.index_map[piece.name]
+				state[index, x, y] = 1 if piece.red else state[index + 7, x, y] = 1
+		if self.red_move:
+			state[14, :, :] = 1
+		for i in range(5):
+			move = self.last_moves[i]
+			if move != ((-1, -1), (-1, -1)):
+				(x_from, y_from), (x_to, y_to) = move
+				state[15 + i, x_from, y_from] = -1
+				state[15 + i, x_to, y_to] = 1
+		return state
 
 	@staticmethod
 	def check_pos(position):
@@ -97,14 +164,8 @@ class Game:
 		:return:
 		"""
 		# copy current chessboard
-		chessboard = deepcopy(self.chessboard)
-
-		(x, y), (x_to, y_to) = move
-		chessboard[x, y].set_position((x_to, y_to))
-		chessboard[x_to, y_to] = chessboard[x, y]
-		chessboard[x, y] = None
-
-		game = Game(chessboard, not self.red_move)
+		game = self.copy()
+		game.move(move)
 		return game
 
 	def check(self):
@@ -154,13 +215,15 @@ class Game:
 		chessboard[x, y].set_position(end)
 		chessboard[x_to, y_to] = chessboard[x, y]
 		chessboard[x, y] = None
-		game = Game(chessboard, self.red_move)
+		game = GameState(chessboard, self.red_move)
 		if game.check():												# 检查是否送将
 			print('不能送将')												# Todo: 把不能送将显示出来
 			return False
 		return True
 
 	def move(self, move):
+		self.last_moves.pop(0)
+		self.last_moves.append(move)
 		(x, y), (x_to, y_to) = move
 		self.chessboard[x, y].set_position((x_to, y_to))
 		self.chessboard[x_to, y_to] = self.chessboard[x, y]
@@ -186,7 +249,7 @@ class Game:
 				chess.set_position(pos)
 				chessboard[x_to, y_to] = chess
 				chessboard[x, y] = None
-				game = Game(chessboard, self.red_move)
+				game = GameState(chessboard, self.red_move)
 				if not game.check():								# 依次检查能否摆脱将军
 					return False
 		return True
